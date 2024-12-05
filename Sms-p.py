80e5ed7be70c8e476b18b066ba91eaac11b148b8
parastoo naderi <roniaania@gmail.com>
2024-12-05 21:37:13 +0330

    definning api and getting the phone nimber from the user

--- Sms-p.py
+++ Sms-p.py
@@ -4,3 +4,10 @@ import random
 
 # تنظیمات Redis
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+#اطلاعات API
+api_key = "SjxROC1gzoyMuZsX7nZiPUB7ae7pkaooI2L3HjkK1GXCNMpx"
+
+
+# گرفتن شماره تلفن از کاربر
+phone_number = input("input your cellphone: ")
