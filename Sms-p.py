017238cf07afe6140cc4464a46953820b623d309
parastoo naderi <roniaania@gmail.com>
2024-12-05 22:42:47 +0330

    i generated a 6-digit code using the random function and cached it in redis

--- Sms-p.py
+++ Sms-p.py
@@ -11,3 +11,15 @@ api_key = "SjxROC1gzoyMuZsX7nZiPUB7ae7pkaooI2L3HjkK1GXCNMpx"
 
 # گرفتن شماره تلفن از کاربر
 phone_number = input("input your cellphone: ")
+
+#گرفتن کد6رقمی
+code = str(random.randint(100000, 999999))
+print(f"Generated code_login: {code}")
+
+
+#کش کردن کد در ردیس برای 5دقیقه(300ثانیه)
+cashed_code =  redis_client.setex(phone_number, 300, code) 
+
+
+
+
