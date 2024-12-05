7383b5551f078e51f1dd8da2c06de70fe9cceb8b
parastoo naderi <roniaania@gmail.com>
2024-12-06 00:03:45 +0330

    The SMS content was received and by defining the send_sms function, the required information including URL, phone number, message content, and API key was provided. To send requests to the server, the post function from the requests library was used, and the server's response was stored in response.

--- Sms-p.py
+++ Sms-p.py
@@ -20,6 +20,31 @@ print(f"Generated code_login: {code}")
 #کش کردن کد در ردیس برای 5دقیقه(300ثانیه)
 cashed_code =  redis_client.setex(phone_number, 300, code) 
 
+#محتوای پیامک 
+message = f"LOGIN CODE: {code}"
+
+#ارسال پیامک
+def send_sms(api_key, phone_number, message):
+    url = "https://api.sms.ir/v1/send" 
+
+    message_data = {
+                   "MobileNumbers": [phone_number],
+                   "Messages": [message],
+                   "LineNumber": "3000773290",
+                  }
+    
+    request_headers = {
+              'Content-Type': 'application/json',
+              'X-API-KEY': api_key
+             }
+    
+    response = requests.post(url, json=message_data, headers=request_headers)
+    # print(f"Response Status Code: {response.status_code}")
+    # print(f"Response Body: {response.text}")
+    return response.json()
+
+
+
 
 
 
